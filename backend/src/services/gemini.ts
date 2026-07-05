13ec713a6c24c2c2375302128fd1cdb2610a98e4
yoniha428 <superrope428@gmail.com>
2026-07-05 09:59:44 +0900

    backendをいじり中

---
+++ backend/src/services/gemini.ts
@@ -0,0 +1,20 @@
+import { GoogleGenAI } from "@google/genai";
+import "dotenv/config"
+
+const apiKey = process.env.GEMINI_API_KEY;
+const ai = new GoogleGenAI({ apiKey });
+
+export async function askGemini(contents: string) {
+  if(!apiKey) return "GEMINI_API_KEYが設定されていません。"
+  try{
+    const interaction = await ai.models.generateContent({
+      model: "gemini-3.1-flash-lite",
+      contents,
+    });
+    return interaction.text;
+  }
+  catch(e){
+    console.error(e);
+    throw e;
+  }
+}
